746d43f4e4cc5842e4b08cf2a279a1cf99b5af1d
Darius Vasionis <darius@vasionis.eu>
2022-11-30 10:26:41 +0200

    Sukuriau admin-ą ir pridėjau goole nuorodą

--- tour_routes/tour/admin.py
+++ tour_routes/tour/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class SightAdmin(admin.ModelAdmin):
+    list_display = ('name', 'street', 'city', 'get_google_link')
+    list_display_links = ('name',)
+    readonly_fields = ('get_google_link',)
+
+
+admin.site.register(models.City)
+admin.site.register(models.Country)
+admin.site.register(models.Route)
+admin.site.register(models.RouteSight)
+admin.site.register(models.TourGuide)
+admin.site.register(models.Sight, SightAdmin)
--- tour_routes/tour/models.py
+++ tour_routes/tour/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
 
 # Standartinis Django user-io modelis
 User = get_user_model()
@@ -9,7 +10,7 @@ User = get_user_model()
 # Pagrindinė informacija apie lankomą šalį
 class Country(models.Model):
     name = models.CharField(_('name'), max_length=255)
-    main_info = models.TextField(_('main_info'))
+    main_info = models.TextField(_('main_info'), blank=True, null=True)
 
     def __str__(self) -> str:
         return self.name
@@ -18,11 +19,11 @@ class Country(models.Model):
 # Pagrindinė informacija apie lankomą/pravažiuojamą miestą
 class City(models.Model):
     name = models.CharField(_('name'), max_length=255)
-    history = models.CharField(_('history'), max_length=255)
-    country_id = models.ForeignKey(Country, 
-    verbose_name=_('country'),
-    on_delete=models.SET_NULL, null=True, blank=True,
-    related_name='route'
+    history = models.CharField(_('history'), max_length=255, blank=True, null=True)
+    country = models.ForeignKey(Country, 
+        verbose_name=_('country'),
+        on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='cities'
     )
 
     def __str__(self) -> str:
@@ -36,15 +37,18 @@ class Sight(models.Model):
     photo = models.ImageField(_('photo'), blank=True, null=True)
     street = models.CharField(_('street'), max_length=255)
     google_link = models.URLField(_('coordinates'), max_length=200, null=True)
-    city_id = models.ForeignKey(
+    city = models.ForeignKey(
         City,
         verbose_name=_('city'),
         on_delete=models.SET_NULL, null=True, blank=True,
-        related_name='city',
+        related_name='sights',
     )
 
     def __str__(self) -> str:
         return self.name
+    
+    def get_google_link(self):
+        return format_html('<a href="{link}">{link}</a>', link=self.google_link)
 
 
 # Kelionės pradžios, pabaigos ir tarpiniai taškai
@@ -72,11 +76,11 @@ class RouteSight(models.Model):
 # Prisijungiantiems asmenims
 class TourGuide(models.Model):
     phone = models.CharField(_('phone'), max_length=20)
-    user_id = models.ForeignKey(
+    user = models.OneToOneField(
         User,
         verbose_name=_('user_id'),
         on_delete=models.SET_NULL, null=True, blank=True,
-        related_name='user_id'
+        related_name='tour_guide'
     )
 
 
