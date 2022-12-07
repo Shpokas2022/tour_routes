10c4821c9dac271b1824fdad951b59ea91bb636c
Darius Vasionis <darius@vasionis.eu>
2022-12-07 09:46:54 +0200

    google link naujame lange

--- tour_routes/tour/views.py
+++ tour_routes/tour/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.views.generic import ListView, DetailView
 from . models import Country, City, Sight, Route, RouteSight, TourGuide
-
+from django.core.paginator import Paginator
+from . import models
 
 def index(request):
     city_count = City.objects.count()
@@ -11,38 +12,46 @@ def index(request):
     visits_count = request.session.get('visits_count', 1)
     request.session['visits_count'] = visits_count + 1
 
-
     context = {
         'city_count': city_count,
         'route_count': route_count,
         'sight_count': sight_count,
         'visits_count': visits_count,
     }
-
     return render(request, 'tour/index.html', context)
 
 def cities(request):
-    return render(request, 'tour/cities.html', {'cities':City.objects.all()})
+    paginator = Paginator(City.objects.all(), 10)
+    page_number = request.GET.get('page')
+    paged_cities = paginator.get_page(page_number)
+    # return render(request, 'tour/cities.html', {'cities':City.objects.all()})
+    return render(request, 'tour/cities.html', {'cities':paged_cities})
 
 def city(request, city_id):
+    # paginator2 = Paginator(City.objects.all(), 5)
+    # page_number = request.GET.get('page')
+    # paged_city = paginator2.get_page(page_number)
     return render(request, 'tour/city.html',{'city':get_object_or_404(City, id=city_id)})
 
-
 def routes(request):
-    return render(request, 'tour/routes.html', {'routes':Route.objects.all()})
-
+    search = request.GET.get('search')
+    if search:
+        routes = routes.filter(sight_facts__icontains=search)
+    paginator = Paginator(Route.objects.all(), 10)
+    page_number = request.GET.get('page')
+    paged_routes = paginator.get_page(page_number)
+    return render(request, 'tour/routes.html', {'routes':paged_routes})
 
 def route(request, route_id):
     return render(request, 'tour/route.html', {'route':get_object_or_404(Route, id=route_id)})
 
-class SightListView(ListView):
-    model = Sight
-    template_name = 'tour/sight_list.html'
+def route_view(request, route_id):
+    return render(request, 'tour/route.html', {'route':get_object_or_404(Route, id=route_id)})
 
-# def get_context_data(self, **kwargs):
-#     context = super().get_context_data(**kwargs)
-#     context["sight_count"] = self.get_queryset().count()
-#     return context
+    
+# class SightListView(ListView):
+#     model = Sight
+#     template_name = 'tour/sight_list.html'
 
 
 class SightDetailView(DetailView):
